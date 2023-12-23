fa0153fff75d05cacf6bc09b6abc2bd3e3f133d8
robbie <robbiecsumner@gmail.com>
2023-12-24 00:37:53 +0100

    implement fitting linear model with gradient descent

--- src/loss_functions.rs
+++ src/loss_functions.rs
@@ -2,35 +2,70 @@ use crate::{
     util::{validate_not_empty, validate_same_length},
     Error,
 };
-use std::iter::zip;
 
 /// Calculates the mean squared error between two vectors.
 ///
 /// # Formula
 ///
-/// MSE = 1/n * sum((y - y_hat)^2)
+/// MSE = 1/n * sum((y_hat - y)^2)
 ///
 /// # Arguments
 ///
-/// * `y` - Vector of actual values
 /// * `y_hat` - Vector of predicted values
-pub fn mean_squared_error(y: &Vec<f64>, y_hat: &Vec<f64>) -> Result<f64, Error> {
-    validate_not_empty(y)?;
+/// * `y` - Vector of actual values
+/// 
+/// # Returns
+/// 
+/// * Mean squared error
+pub fn mean_squared_error(y_hat: &Vec<f64>, y: &Vec<f64>) -> Result<f64, Error> {
     validate_not_empty(y_hat)?;
-    validate_same_length(y, y_hat)?;
+    validate_not_empty(y)?;
+    validate_same_length(y_hat, y)?;
 
-    Ok(_mean_squared_error(y, y_hat))
+    Ok(_mean_squared_error(y_hat, y))
 }
 
 fn _mean_squared_error(y: &Vec<f64>, y_hat: &Vec<f64>) -> f64 {
-    let sum = zip(y, y_hat)
-        .into_iter()
-        .map(|(y, y_hat)| (y - y_hat).powi(2))
+    let sum = y_hat
+        .iter()
+        .zip(y)
+        .map(|(y_hat, y)| (y_hat - y).powi(2))
         .collect::<Vec<f64>>()
         .iter()
         .sum::<f64>();
 
     sum / y.len() as f64
+}
+
+/// Calculates the gradient of the mean squared error between two vectors.
+/// 
+/// # Formula
+/// 
+/// d/dy_hat MSE = 2/n * (y_hat - y)
+/// 
+/// # Arguments
+/// 
+/// * `y_hat` - Vector of predicted values
+/// * `y` - Vector of actual values
+/// 
+/// # Returns
+/// 
+/// * Vector of partial derivatives of MSE with respect to each y_hat
+pub fn gradient_mse(y_hat: &Vec<f64>, y: &Vec<f64>) -> Result<Vec<f64>, Error> {
+    validate_not_empty(y_hat)?;
+    validate_not_empty(y)?;
+    validate_same_length(y_hat, y)?;
+
+    Ok(_gradient_mse(y_hat, y))
+}
+
+fn _gradient_mse(y_hat: &Vec<f64>, y: &Vec<f64>) -> Vec<f64> {
+    let n = y.len() as f64;
+
+    y_hat.iter()
+        .zip(y)
+        .map(|(y_hat, y)| 2.0 / n * (y_hat - y))
+        .collect()
 }
 
 #[cfg(test)]
@@ -75,5 +110,16 @@ mod tests {
         let y_hat: Vec<f64> = vec![2.0, 3.0, 4.0];
 
         assert_eq!(mean_squared_error(&y, &y_hat).unwrap(), 1.0);
+    }
+
+    #[test]
+    fn test_gradient_mse() {
+        let y_hat: Vec<f64> = vec![2.0, 3.0, 4.0];
+        let y: Vec<f64> = vec![1.0, 2.0, 3.0];
+
+        assert_eq!(
+            gradient_mse(&y_hat, &y).unwrap(),
+            vec![2.0 / 3.0, 2.0 / 3.0, 2.0 / 3.0]
+        );
     }
 }
--- src/models.rs
+++ src/models.rs
@@ -1,4 +1,5 @@
 use crate::{
+    loss_functions::gradient_mse,
     util::{validate_not_empty, validate_same_length},
     Error,
 };
@@ -25,6 +26,10 @@ impl LinearModel {
     }
 
     /// Fit linear model.
+    /// 
+    /// # Note
+    /// 
+    /// Does not fit intercept for now! # TODO
     ///
     /// # Arguments
     ///
@@ -34,8 +39,13 @@ impl LinearModel {
     /// # Requirements
     ///
     /// * `x` must have the same length as `y`
-    /// * `x`'s rows must all be same length (not implemented yet!)
-    pub fn fit(self, x: &Vec<Vec<f64>>, y: &Vec<f64>) -> Result<Self, Error> {
+    /// * `x`'s rows must all be same length
+    /// 
+    /// # Returns
+    /// 
+    /// * `Ok(&Self)` if successful
+    /// * `Err(Error)` if unsuccessful
+    pub fn fit(&mut self, x: &Vec<Vec<f64>>, y: &Vec<f64>) -> Result<&Self, Error> {
         validate_not_empty(x)?;
         validate_not_empty(y)?;
 
@@ -47,11 +57,40 @@ impl LinearModel {
             .skip(1)
             .try_for_each(|row| validate_same_length(first_row, row))?;
 
-        Ok(self._fit(x, y))
+        Ok(self._fit(x, y, 1000, 0.01, 0.000001))
     }
 
-    fn _fit(self, _x: &Vec<Vec<f64>>, _y: &Vec<f64>) -> Self {
-        // TODO: implement fit
+    fn _fit(
+        &mut self,
+        x: &Vec<Vec<f64>>,
+        y: &Vec<f64>,
+        max_iter: u32,
+        learning_rate: f64,
+        tol: f64,
+    ) -> &Self {
+        // initialize coefficients to 0
+        self.coefficients = vec![0.0; x.first().unwrap().len()];
+
+        for _ in 0..max_iter {
+            let y_hat = self.predict(x).unwrap();
+            let gradient = gradient_mse(&y_hat, y).unwrap();
+            // check if converged
+            if gradient.iter().all(|g| g.abs() < tol) {
+                break;
+            }
+
+            // update coefficients
+            self.coefficients = self
+                .coefficients
+                .iter()
+                .zip(&gradient)
+                .map(|(a, g)| a - learning_rate * g)
+                .collect();
+
+            // update intercept
+            // TODO: how to update intercept?
+        }
+
         self
     }
 
@@ -63,7 +102,12 @@ impl LinearModel {
     ///
     /// # Requirements
     ///
-    /// * `x`'s rows must all match the length of the coefficients of the `LinearModel` (not implemented yet! only checks first row for now)
+    /// * `x`'s rows must all match the length of the coefficients of the `LinearModel`
+    /// 
+    /// # Returns
+    /// 
+    /// * `Ok(Vec<f64>)` if successful
+    /// * `Err(Error)` if unsuccessful
     pub fn predict(&self, x: &Vec<Vec<f64>>) -> Result<Vec<f64>, Error> {
         validate_not_empty(x)?;
 
@@ -119,10 +163,28 @@ mod tests {
 
     #[test]
     fn test_fit() {
-        let x: Vec<Vec<f64>> = vec![vec![1.0, 2.0, 3.0]; 3];
-        let y: Vec<f64> = vec![0.0; 3];
+        let x = vec![vec![1.0], vec![2.0]];
+        let y = vec![1.0, 2.0];
 
-        assert!(LinearModel::new().fit(&x, &y).is_ok());
+        let mut model = LinearModel::new();
+
+        model.fit(&x, &y).unwrap();
+
+        println!("{:?}", model);
+        println!("{:?}", model.predict(&x).unwrap());
+    }
+
+    #[test]
+    fn test_fit_with_intercept() {
+        let x = vec![vec![1.0], vec![2.0]];
+        let y = vec![2.0, 4.0];
+
+        let mut model = LinearModel::new();
+
+        model.fit(&x, &y).unwrap();
+
+        println!("{:?}", model);
+        println!("{:?}", model.predict(&x).unwrap());
     }
 
     #[test]
@@ -158,6 +220,19 @@ mod tests {
 
         let x: Vec<Vec<f64>> = vec![vec![1.0], vec![2.0], vec![3.0]];
         let expected: Vec<f64> = vec![2.0, 3.0, 4.0];
+
+        assert_eq!(model.predict(&x).unwrap(), expected);
+    }
+
+    #[test]
+    fn test_predict_with_multiple_coefficients() {
+        let mut model = LinearModel::new();
+
+        model.intercept = 1.0;
+        model.coefficients = vec![1.0, 2.0];
+
+        let x: Vec<Vec<f64>> = vec![vec![1.0, 1.0], vec![2.0, 2.0], vec![3.0, 3.0]];
+        let expected: Vec<f64> = vec![4.0, 7.0, 10.0];
 
         assert_eq!(model.predict(&x).unwrap(), expected);
     }
